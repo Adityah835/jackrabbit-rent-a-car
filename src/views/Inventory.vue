f09ead2ae96667d179af5893c8c1570a42df2d9b
Aditya Harshvardhan <aditya.harshvardhan@jacks.sdstate.edu>
2020-11-15 00:17:12 -0600

    Modified the Inventory UI

--- src/views/Inventory.vue
+++ src/views/Inventory.vue
@@ -7,7 +7,10 @@
             <div v-if = "carInventory.length">
                 <div v-for = "(carInvent, index) in carInventory" :key="index" :title="carInvent.plateNo" class = "post">
                     <h5> {{carInvent.make}} {{carInvent.model}} </h5>
-                    <p> $ {{carInvent.rentRate}} </p>
+                    <p> <strong> Color : </strong> {{carInvent.color}} </p>
+                    <p> <strong> License Plate No : </strong> {{carInvent.plateNo}} </p>
+                    <p> <strong> Location : </strong> {{carInvent.location}} </p>
+                    <p> <strong> Type : </strong> {{carInvent.type}} </p>
                 </div>
             </div>
         </div>
